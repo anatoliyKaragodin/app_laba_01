cded34c990353d28775079923922947933fbc1e5
anatoliyKaragodin <anatoliy.karagodin@gmail.com>
2022-11-22 12:58:42 +0400

    0.05 (5 screens + navigation + all elements + login and password + thme change)

--- lib/main.dart
+++ lib/main.dart
@@ -4,22 +4,49 @@ import 'package:app_laba_01/screens/login_email_page.dart';
 import 'package:app_laba_01/screens/login_google_page.dart';
 import 'package:app_laba_01/screens/settings_page.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:app_laba_01/settings/model_theme.dart';
 
 void main() {
-  runApp(MaterialApp(
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(),
-      themeMode: ThemeMode.dark,
-      darkTheme: ThemeData(brightness: Brightness.dark),
+  runApp(MyApp());
+}
 
-      /// Routes of application
-      ///
-      initialRoute: '/home page',
-      routes: {
-        '/home page': (context) => const HomePage(),
-        '/login Email': (context) => const LoginEmailPage(),
-        '/login Google': (context) => const LoginGooglePage(),
-        '/app page': (context) => const AppPage(),
-        '/settings page': (context) => const SettingsPage(),
-      }));
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return ChangeNotifierProvider(
+      create: (_) => ModelTheme(),
+      child: Consumer<ModelTheme>(
+          builder: (context, ModelTheme themeNotifier, child) {
+            return MaterialApp(
+                debugShowCheckedModeBanner: false,
+              theme: themeNotifier.isDark
+                  ? ThemeData(
+                brightness: Brightness.dark,
+              )
+                  : ThemeData(
+                  brightness: Brightness.light,
+
+                  // primarySwatch: Colors.orange
+              ),
+                // themeMode: ThemeMode.dark,
+                // darkTheme: ThemeData(brightness: Brightness.dark),
+
+                /// Routes of application
+                ///
+                initialRoute: '/home page',
+                routes: {
+                  '/home page': (context) => const HomePage(),
+                  '/login Email': (context) => const LoginEmailPage(),
+                  '/login Google': (context) => const LoginGooglePage(),
+                  '/app page': (context) => const AppPage(),
+                  '/settings page': (context) => const SettingsPage(),
+                },
+              title: 'Flutter Demo',
+
+              home:const HomePage(),
+            );
+          }),
+    );
+  }
 }
--- lib/screens/settings_page.dart
+++ lib/screens/settings_page.dart
@@ -1,54 +1,72 @@
 import 'package:flutter/material.dart';
+import 'package:app_laba_01/settings/model_theme.dart';
+import 'package:provider/provider.dart';
 
 class SettingsPage extends StatelessWidget {
   const SettingsPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(
-          automaticallyImplyLeading: false,
-          actions: [
-            /// Close icon(return to app_page)
-            ///
-            IconButton(
-              icon: const Icon(Icons.close),
-              onPressed: () {
-                Navigator.pushNamed(context, '/app page');
-              },
-            ),
-          ],
-        ),
-        body: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            /// Application theme option
-            ///
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                const Text('Тема приложения'),
-                IconButton(
-                  onPressed: () {},
-                  icon: const Icon(Icons.light_mode),
-                ),
-                IconButton(
-                  onPressed: () {},
-                  icon: const Icon(Icons.dark_mode),
-                ),
-              ],
-            ),
-            /// Application language option
-            ///
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                TextButton(onPressed: () {}, child: const Text('Русский')),
-                TextButton(onPressed: () {}, child: const Text('English')),
-                TextButton(onPressed: () {}, child: const Text('中国人')),
-              ],
-            ),
-          ],
-        ));
+    return Consumer<ModelTheme>(
+        builder: (context, ModelTheme themeNotifier, child)
+    {
+      return Scaffold(
+          appBar: AppBar(
+            automaticallyImplyLeading: false,
+            actions: [
+
+              /// Close icon(return to app_page)
+              ///
+              IconButton(
+                icon: const Icon(Icons.close),
+                onPressed: () {
+                  Navigator.pushNamed(context, '/app page');
+                },
+              ),
+            ],
+          ),
+          body: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+
+              /// Application theme option
+              ///
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const Text('Тема приложения'),
+                  IconButton(
+                    onPressed: () {
+                      themeNotifier.isDark
+                          ? themeNotifier.isDark = false
+                          : themeNotifier.isDark = true;
+                    },
+                    icon: Icon(
+                        themeNotifier.isDark
+                            ? Icons.nightlight_round
+                            : Icons.wb_sunny),
+                  ),
+                  // IconButton(
+                  //   onPressed: () {},
+                  //   icon: const Icon(Icons.dark_mode),
+                  // ),
+                ],
+              ),
+
+              /// Application language option
+              ///
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  TextButton(onPressed: () {}, child: const Text('Русский')),
+                  TextButton(onPressed: () {}, child: const Text('English')),
+                  TextButton(onPressed: () {}, child: const Text('中国人')),
+                ],
+              ),
+            ],
+          )
+      );
+    }
+    );
   }
 }
